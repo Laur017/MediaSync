75672f25db4d73eb48f7038d6878d96bc22ceb3e
Laurentiu Popa <popa.laur.01@gmail.com>
2023-08-07 15:23:16 +0300

    The beggin

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <h3>Media Sync</h3>
+        <button>Log in</button>
+    </nav>
+  )
+}

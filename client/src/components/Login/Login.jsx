4b6b8f100e042165f741ac7876dcb76cdc2d27fc
Laurentiu Popa <popa.laur.01@gmail.com>
2023-08-10 16:42:39 +0300

    Update Login.jsx

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+  const navigate = useNavigate()
   return (
     <div className='login-div'>
         <h3>Log in</h3>
         <input type="text" placeholder='username' />
         <input type="text" placeholder='password' />
-        <button>Sign in</button>
+        <button onClick={()=>navigate("/main")}>Sign in</button>
     </div>
   )
 }
